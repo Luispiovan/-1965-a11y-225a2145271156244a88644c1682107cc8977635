39fd919b6c909226ca5991a14c58994747adb040
Grupo03 - LuisPiovan <ex150351@g.unicamp.br>
2021-05-27 11:50:19 -0300

    included of interface class the propertys binding and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { YesNoButtonModule } from './shared/components/yes-no-button/yes-no-button.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, YesNoButtonModule],
+  imports: [BrowserModule, YesNoButtonModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/components/yes-no-button/yes-no-button.component.ts
+++ src/app/shared/components/yes-no-button/yes-no-button.component.ts
@@ -1,24 +1,58 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  Output,
+  EventEmitter,
+  forwardRef,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-yes-no-button',
   templateUrl: './yes-no-button.component.html',
   styleUrls: ['./yes-no-button.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => YesNoButtonComponent),
+    },
+  ],
 })
-export class YesNoButtonComponent implements OnInit {
+export class YesNoButtonComponent implements OnInit, ControlValueAccessor {
   @Input() public value: string = null;
   @Input() public label = '';
   @Output() public valueChange = new EventEmitter<string>();
 
   public option = YesNoButtonOptions;
+  public onChange = (value: string) => {};
+  public onTouched = () => {};
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  activate(value: string): void {
+  public writeValue(value: string): void {
     this.value = value;
+    this.onChange(this.value);
     this.valueChange.emit(this.value);
+  }
+
+  public registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
+
+  activate(value: string): void {
+    this.writeValue(value);
   }
 }
 
